8e955d8ebf57dce3054e4a8f0d6901e49f955c4b
«HaykOsyan <ohayk@mail.ru>
2024-10-17 17:57:23 +0400

    First commit version 1.01

--- Notebook.java
+++ Notebook.java
@@ -1,9 +1,7 @@
 package java_atestat;
 
-import java.util.HashMap;
-import java.util.ArrayList;
-
 public class Notebook {
+
     // Задание 1. Поиск ноутбуков
     // Подумать над структурой класса Ноутбук для магазина техники - выделить поля и
     // методы.
@@ -34,27 +32,36 @@ public class Notebook {
                 + "', screenSize='" + screenSize + "'}";
     }
 
-    HashMap<Integer, ArrayList<Notebook>> notebooks = new HashMap<>();
-
-    public void addNoteBook(int id, String model, String name, String ram, String hdd, String screenSize) {
-        if (notebooks.containsKey(id)) {
-            System.out.println("There is notebook with such ID in shop");
-        }
-        ArrayList<Notebook> newList = new ArrayList<>();
-        newList.add(new Notebook(id, model, name, ram, hdd, screenSize));
-        notebooks.put(id, newList);
-    }
-    
     // public Notebook getByName(String name) {
-    //     for (ArrayList<Notebook> notebookList : notebooks.values()) {
-    //         for (Notebook notebook : notebookList) {
-    //             if (notebook.name.equals(name)) {
-    //                 return notebook;
-    //             }
-    //         }
-    //     }
-    //     System.out.println("There is no notebook with such name");
-    //     return null;
+    // for (ArrayList<Notebook> notebookList : notebooks.values()) {
+    // for (Notebook notebook : notebookList) {
+    // if (notebook.name.equals(name)) {
+    // return notebook;
+    // }
+    // }
+    // }
+    // System.out.println("There is no notebook with such name");
+    // return null;
     // }
 
+    public static void main(String[] args) {
+        NotebookController controller = new NotebookController();
+        controller.addNotebook(1, "Dell", "XPS", "16GB", "512GB", "13.3\"");
+        controller.addNotebook(2, "HP", "Spectre", "16GB", "1TB", "15.6\"");
+
+        Notebook foundNotebook = controller.getBy("name", "XPS");
+        if (foundNotebook != null) {
+            System.out.println(foundNotebook);
+        } else {
+            System.out.println("Notebook not found.");
+        }
+
+        Notebook hddNotebook = controller.getBy("hdd", "1TB");
+        if (hddNotebook != null) {
+            System.out.println(hddNotebook);
+        } else {
+            System.out.println("Notebook not found.");
+        }
+
+    }
 }
---
+++ NotebookController.java
@@ -0,0 +1,60 @@
+package java_atestat;
+
+import java.util.HashMap;
+
+public class NotebookController {
+
+    HashMap<Integer, Notebook> notebooks = new HashMap<>();
+
+    // in future ID must be auto setable
+    // Check
+    public Notebook addNotebook(int id, String model, String name, String ram, String hdd, String screenSize) {
+        if (notebooks.containsKey(id)) {
+            System.out.println("There is a notebook with such id");
+            return null;
+        }
+        Notebook notebook = new Notebook(id, model, name, ram, hdd, screenSize);
+        notebooks.put(id, notebook);
+        // is there 2 time typing ID??
+        // CHECK
+        System.out.println("New notebook has been added");
+        return notebook;
+    }
+
+    // by ---- name, ram, hdd, screenSize
+    public Notebook getBy(String by, String attribute) {
+        // in this method in future add List of founded notebooks, there can be more
+        // than one found ...
+        // Check
+        for (Notebook notebook : notebooks.values()) {
+            switch (by) {
+                case "name":
+                    if (notebook.name.equals(attribute)) {
+                        return notebook;
+                    }
+                    break;
+                case "ram":
+                    if (notebook.ram.equals(attribute)) {
+                        return notebook;
+                    }
+                    break;
+                case "hdd":
+                    if (notebook.hdd.equals(attribute)) {
+                        return notebook;
+                    }
+                    break;
+                case "screenSize":
+                    if (notebook.screenSize.equals(attribute)) {
+                        return notebook;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid attribute specified");
+                    return null;
+            }
+        }
+        System.out.println("There is no notebook with " + by + "-----" + attribute);
+        return null;
+    }
+
+}
